51b588e0d1cdae2b6ed78f79894cfd3d6345ac80
inho9377 <inho9377@naver.com>
2022-08-06 00:02:33 +0900

    Upload Package

--- fastcampus_NodeJsServer/009_cli/src/n.js
+++ fastcampus_NodeJsServer/009_cli/src/n.js
@@ -1,11 +1,41 @@
 console.log(process.argv)
 const { program } = require('commander');
+const { Octokit } = require('octokit')
+
+const octokit = new Octokit({auth: ProcessingInstruction.env.GitHuc_Access_PToken})
 
 program
-  .option('--first')
-  .option('-s, --separator <char>');
+  .command('me')
+  .description('Check my Profile')
+  .action(async () => {
+    const {
+      data: { login }
+    } = await octokit.rest.users.getAuthenticated()
+    console.log(`${login}`)
+  })
 
-program.parse();
+
+program
+  .command('list-bugs')
+  .description('List issuses with bug label')
+  .action(async () => {
+
+    const result = await octokit.rest.issues.listForRepo({
+      owner: 'inho9377',
+      repo: 'reponame'
+    })
+
+    console.log(`List bugs! ${result}`)
+  })
+
+program
+  .command('check-prs')
+  .description('Check pull')
+  .action(async () => {
+    console.log('check prs')
+  })
+
+program.parseAsync();
 
 const options = program.opts();
 const limit = options.first ? 1 : undefined;
