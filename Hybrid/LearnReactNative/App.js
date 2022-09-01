43a01bd5207aaf5468c4754e384f1d833b2fdba0
inho9377 <inho9377@naver.com>
2022-09-01 17:27:22 +0900

    react native

--- Hybrid/LearnReactNative/App.js
+++ Hybrid/LearnReactNative/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Button} from 'react-native';
+import {SafeAreaView, Text, View, StatusBar} from 'react-native';
+import Box from './components/Box';
 import Greeting from './components/Greeting';
 
 const App = () => {
+  const [visible, setVisible] = useState(true);
+  const onPress = () => {
+    setVisible(!visible);
+  };
   return (
-    <SafeAreaView>
-      <View>
-        <Greeting />
-      </View>
-    </SafeAreaView>
+    <>
+      <StatusBar backgroundColor="#26a69a" />
+      <SafeAreaView style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'blue'}} />
+        <View>
+          <Button title="토글" onPress={onPress} />
+          {/* {visible? <Box rounded={true} size="large" /> : null} */}
+          {visible && <Box rounded={true} size="large" />}
+        </View>
+      </SafeAreaView>
+    </>
   );
 };
 
